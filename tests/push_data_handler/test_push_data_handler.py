0df61b220a0e65545f229357712bbafb3133e974
Durraiyah Muneer <durraiyah.munir@xloopdigital.com>
2023-02-10 15:06:52 +0500

    version 1 test_push_data_handler.py

--- tests/push_data_handler/test_push_data_handler.py
+++ tests/push_data_handler/test_push_data_handler.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-
 from fastapi.testclient import TestClient
 
 from Capstone_project_4.push_data_handler.push_data_handler import app
@@ -7,39 +5,26 @@ from Capstone_project_4.push_data_handler.push_data_handler import app
 client = TestClient(app)
 
 
-class TestLog(TestCase):
-    def test_collect_moisture_mate_data_response_200_and_logs(self):
-        with self.assertLogs() as captured:
-            response = client.post(
-                "/api/collect/moisturemate_data", json={"test_key": "test_value"}
-            )
-            assert response.status_code == 200
-            # check that there is only one message
-            self.assertEqual(len(captured.records), 1)
-            # check that the correct mesage is logged
-            self.assertEqual(
-                captured.records[0].getMessage(),
-                "Received MoistureMate Data: {'test_key': 'test_value'}",
-            )
+def test_collect_moisture_mate_data_response_200():
+    response = client.post(
+        "/api/collect/moisturemate_data",
+        json={"test_key": "test_value"},
+    )
+    assert response.status_code == 200
 
-    def test_collect_carbonsense_data_response_200_and_logs(self):
-        with self.assertLogs() as captured:
-            response = client.post(
-                "/api/collect/carbonsense_data",
-                json={"test_key": "test_value"},
-            )
-        assert response.status_code == 200
-        # check that there is only one message
-        self.assertEqual(len(captured.records), 1)
-        # check that the correct mesage is logged
-        self.assertEqual(
-            captured.records[0].getMessage(),
-            "Received CarbonSense Data: {'test_key': 'test_value'}",
-        )
 
-    def test_invalid_endpoints_response_404(self):
-        response = client.post(
-            "/api/invalid",
-            json={"test_key": "test_value"},
-        )
-        assert response.status_code == 404
+def test_collect_carbonsense_data_response_200():
+
+    response = client.post(
+        "/api/collect/carbonsense_data",
+        json={"test_key": "test_value"},
+    )
+    assert response.status_code == 200
+
+
+def test_invalid_endpoints_response_404():
+    response = client.post(
+        "/api/invalid",
+        json={"test_key": "test_value"},
+    )
+    assert response.status_code == 404
