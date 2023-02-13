87a931cc7c573f05c6f75d4c0d4eb128d8bba43b
shamza0 <syed.hamza@xloopdigital.com>
2023-02-13 05:47:56 +0500

    updated 'lux_data_extraction_job' script with required changes

--- lux_data_puller/lux_data_extraction_job.py
+++ lux_data_puller/lux_data_extraction_job.py
@@ -1,15 +1,36 @@
+import json
 import logging
 import os
+import time
 
 import requests
+from kafka import KafkaProducer
 
 logger = logging.getLogger()
 
-
-rooms = ["kitchen", "bedroom", "bathroom", "living_room"]
 url_prefix = os.environ.get("SENSORSMOCK_URL")
 
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=lambda x: json.dumps(x).encode("utf8"),
+    api_version=(0, 10, 1),
+)
 
-for room in rooms:
-    response = requests.get(f"{url_prefix}/{room}").json()
-    logger.info(f"Received luxmeter data: {response}")
+
+def get_lux_data(room):
+    data = requests.get(f"{url_prefix}/{room}").json()
+    return {data["room_id"]: data["measurements"][-1]}
+
+
+def send_to_producer():
+    rooms = ["kitchen", "bedroom", "bathroom", "living_room"]
+    for room in rooms:
+        lux_data = get_lux_data(room)
+        producer.send("luxmeter", value=lux_data)
+        logger.info(f"Sent luxmeter data: {lux_data}")
+
+
+while True:
+    send_to_producer()
+    time.sleep(60)
