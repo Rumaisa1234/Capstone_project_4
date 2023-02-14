3514c7491e433d6871262a76c6f9ead627a2a0fb
yamnatahir-2211-034-KHI-DEG <yamna.tahir@xloopdigital.com>
2023-02-14 15:17:28 +0500

    adding consumer tests for moisturemate and carbonsense

---
+++ consumers/carbonsense.py
@@ -0,0 +1,29 @@
+import json
+import logging
+
+from kafka import KafkaConsumer
+
+logger = None
+consumer = None
+
+
+def json_deserializer(data):
+    return json.loads(data.decode("utf-8"))
+
+
+def read_msgs():
+    for message in consumer:
+        logger.info(f"Received Carbonsense Data: {message.value}")
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    consumer = KafkaConsumer(
+        "carbonsense",
+        bootstrap_servers=["localhost:9092"],
+        value_deserializer=json_deserializer,
+        auto_offset_reset="latest",
+        enable_auto_commit=True,
+    )
+    logging.basicConfig(level=logging.INFO)
+    read_msgs()
