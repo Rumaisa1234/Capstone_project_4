7fc758403677c46de8a0723cd168368cb7a875ed
yamnatahir-2211-034-KHI-DEG <yamna.tahir@xloopdigital.com>
2023-02-14 15:43:50 +0500

    implementing requested changes

--- smart_thermo_service/smart_thermo_service.py
+++ smart_thermo_service/smart_thermo_service.py
@@ -13,11 +13,8 @@ KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 KAFKA_TOPIC = os.environ.get("KAFKA_TOPIC")
 
 
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BROKER_URL,
-    value_serializer=lambda x: json.dumps(x).encode("utf8"),
-    api_version=(0, 10, 1),
-)
+producer=None
+
 
 s3 = boto3.client(
     "s3",
@@ -41,8 +38,16 @@ def get_data():
     initial_df = initial_df.iloc[:, 1:]
     return initial_df.to_dict(orient="records")
 
+def loop():
+    while True:
+        data = get_data()
+        producer.send(KAFKA_TOPIC, value=data)
+        time.sleep(60)
+        
+if __name__ == "__main__":
+    producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=lambda x: json.dumps(x).encode("utf8"),
+    api_version=(0, 10, 1),)
 
-while True:
-    data = get_data()
-    producer.send(KAFKA_TOPIC, value=data)
-    time.sleep(60)
+    loop()
