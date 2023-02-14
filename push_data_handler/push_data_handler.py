ad04cb3cb5ca3adfb62fdf16105699c1305ee99a
yamnatahir-2211-034-KHI-DEG <yamna.tahir@xloopdigital.com>
2023-02-14 15:09:22 +0500

    sending post api data for carbonsense and moisturemate to kafka

--- push_data_handler/push_data_handler.py
+++ push_data_handler/push_data_handler.py
@@ -1,24 +1,30 @@
+import os
 import logging
-
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import Request, FastAPI
+from kafka import KafkaProducer
+
+import json
+# Kafka settings
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
+producer=None
+logger=None
+
 
 app = FastAPI()
-
-
 @app.post("/api/collect/moisturemate_data")
 async def collect_moisture_mate_data(request: Request):
     received_data = await request.json()
-    print(f"Received MoistureMate Data: {received_data}")
-    return {"msg": "received moisture_mate data"}
-
+    logger.info(f"Received MoistureMate Data: {received_data}")
+    record = producer.send('moisturemate', value=received_data)    
+    logger.info('Producer has not sent your message')
 
 @app.post("/api/collect/carbonsense_data")
-async def collect_carbonsense_data(request: Request):
+async def collect_carbon_sense_data(request: Request):
     received_data = await request.json()
-    print(f"Received carbonsense Data: {received_data}")
-    return {"msg": "received carbonsense data"}
-
+    logger.info(f"Received CarbonSense Data: {received_data}")
+    record = producer.send('carbonsense', value=received_data)    
+    logger.info('Producer has not sent your message')
 
 def run_app():
     logging.basicConfig(level=logging.INFO)
@@ -26,4 +32,11 @@ def run_app():
 
 
 if __name__ == "__main__":
+    producer = KafkaProducer(
+            bootstrap_servers=KAFKA_BROKER_URL,
+            value_serializer=lambda x: json.dumps(x).encode('utf8'),
+            api_version=(0, 10, 1)
+        )
+    logger=logging.getLogger()
     run_app()
+
