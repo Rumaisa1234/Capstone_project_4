dad7c9cbb56a0c87c7216a8468ddc61fad2f4f15
shamza0 <syed.hamza@xloopdigital.com>
2023-02-17 11:53:25 +0500

    adding APscheduler in the smart thermo service script

--- smart_thermo_service/smart_thermo_service.py
+++ smart_thermo_service/smart_thermo_service.py
@@ -1,17 +1,20 @@
-import datetime
 import io
 import json
+import logging
 import os
 import time
+from datetime import datetime
 
 import boto3
 import pandas as pd
+from apscheduler.schedulers.blocking import BlockingScheduler
 from kafka import KafkaProducer
 
 SMART_THERMO_BUCKET = os.environ.get("SMART_THERMO_BUCKET")
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 KAFKA_TOPIC = os.environ.get("KAFKA_TOPIC")
 
+logger = logging.getLogger()
 
 producer = None
 
@@ -25,25 +28,38 @@ s3 = boto3.client(
 
 
 def get_datetime():
-    time_now = datetime.datetime.utcnow().replace(microsecond=0, second=0).isoformat()
+    time_now = datetime.utcnow().replace(microsecond=0, second=0).isoformat()
     return f"{time_now}.csv"
 
 
 def get_data():
-    bucket = SMART_THERMO_BUCKET
+    bucket = "smartthermo"
     key = get_datetime()
-    obj = s3.get_object(Bucket=bucket, Key=f"smart_thermo/{key}")
-    res = obj["Body"].read()
-    initial_df = pd.read_csv(io.BytesIO(res))
-    initial_df = initial_df.iloc[:, 1:]
-    return initial_df.to_dict(orient="records")
+    second = 0
+    while second < 11:
+        try:
+            obj = s3.get_object(Bucket=bucket, Key=f"smart_thermo/{key}")
+            res = obj["Body"].read()
+            initial_df = pd.read_csv(io.BytesIO(res))
+            initial_df = initial_df.iloc[:, 1:]
+            return initial_df.to_dict(orient="records")
+        except Exception:
+            logger.info("not a new value")
+            time.sleep(1)
+            second += 1
 
 
-def loop():
-    while True:
-        data = get_data()
-        producer.send(KAFKA_TOPIC, value=data)
-        time.sleep(60)
+def send_to_producer():
+    data = get_data()
+    producer.send("smart_thermo", value=data)
+
+
+def scheduling():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(
+        send_to_producer, "cron", second="0", next_run_time=datetime.now()
+    )
+    scheduler.start()
 
 
 if __name__ == "__main__":
@@ -53,4 +69,4 @@ if __name__ == "__main__":
         api_version=(0, 10, 1),
     )
 
-    loop()
+    scheduling()
