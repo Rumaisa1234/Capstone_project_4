a88209fbf6c629fdab5ef14bfd2c6f3a314088e0
shamza0 <syed.hamza@xloopdigital.com>
2023-02-17 11:51:46 +0500

    adding APscheduler to lux data extraction script

--- lux_data_puller/lux_data_extraction_job.py
+++ lux_data_puller/lux_data_extraction_job.py
@@ -2,8 +2,10 @@ import json
 import logging
 import os
 import time
+from datetime import datetime
 
 import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
 from kafka import KafkaProducer
 
 url_prefix = os.environ.get("SENSORSMOCK_URL")
@@ -15,8 +17,17 @@ producer = None
 
 
 def get_lux_data(room):
-    data = requests.get(f"{url_prefix}/{room}").json()
-    return {data["room_id"]: data["measurements"][-1]}
+    seconds = 0
+    while seconds < 11:
+        my_date = datetime.utcnow().replace(second=0, microsecond=0).isoformat()
+        data = requests.get(f"{url_prefix}/{room}").json()
+        timestamp = data["measurements"][-1]["timestamp"]
+        if my_date == timestamp:
+            return {data["room_id"]: data["measurements"][-1]}
+        else:
+            logger.info("not a new value")
+            time.sleep(1)
+            seconds += 1
 
 
 def send_to_producer():
@@ -27,10 +38,12 @@ def send_to_producer():
         logger.info(f"Sent luxmeter data: {lux_data}")
 
 
-def loop():
-    while True:
-        send_to_producer()
-        time.sleep(60)
+def scheduling():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(
+        send_to_producer, "cron", second="0", next_run_time=datetime.now()
+    )
+    scheduler.start()
 
 
 if __name__ == "__main__":
@@ -43,4 +56,4 @@ if __name__ == "__main__":
         api_version=(0, 10, 1),
     )
 
-    loop()
+    scheduling()
